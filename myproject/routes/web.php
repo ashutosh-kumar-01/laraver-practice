db3d6dbd55c4977d0a7aa4f0b781dea0f9ff2538
ashutosh7761 <goluashu3@gmail.com>
2026-06-05 15:20:30 +0530

    l

--- myproject/routes/web.php
+++ myproject/routes/web.php
@@ -2,44 +2,99 @@
 
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\DB;
-use App\Http\Controllers\StudentController;
-use App\Http\Controllers\APIController;
-use App\Http\Controllers\StudentFormController;
-use App\Http\Controllers\SendEmail;
-use App\Http\Controllers\SendFileController;
-use App\Http\Controllers\CookieController;
-use App\Http\Controllers\DataRetrival;
-use App\Http\Controllers\Emailtwo;
-use Illuminate\Http\Request;
-use App\Http\Controllers\ProductController;
-use App\Http\Controllers\groceryshop;
-use App\Http\Controllers\University;
-
 use App\Http\Controllers\FormStore;
+use App\Http\Controllers\productController;
+
+
+Route::get('/showdataa', [productController::class, 'index']);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 Route::get('/student-form', [FormStore::class, 'create']);
 Route::post('/student-store', [FormStore::class, 'store']);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 Route::get('/showdata', [FormStore::class, 'showdata']);
+
